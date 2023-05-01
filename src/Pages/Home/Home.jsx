acd49f1ac4efab0203b0e21067917e2a47638946
aminul-islam01 <abiddut953@gmail.com>
2023-05-01 21:15:32 +0600

    add travel places card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,39 +16,56 @@ import img1 from '../../assets/Sajek.png'
 import img2 from '../../assets/Sreemongol.png'
 import img3 from '../../assets/sundorbon.png'
 import bgc from '../../assets/Rectangle1.png'
+import { Button, Col, Row } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+    const travels = useLoaderData()
+
     const [swiperRef, setSwiperRef] = useState(null);
 
-    const [url, setUrl] = useState(bgc);
-    const [text, setText] = useState();
+    // const [url, setUrl] = useState(bgc);
+    const [name, setName] = useState("Cox's Bazar");
+    const [description, setDescription] = useState();
 
-    const handleBackgroundImg = (imgUrl) => {
-        setUrl(imgUrl);
-        // setText(text)
+    const handleBackgroundImg = (travel) => {
+        // setUrl(imgUrl);
+        setName(travel.name)
     }
-   
+    console.log(swiperRef)
     return (
-        <div>
-            <Swiper
-                onSwiper={setSwiperRef}
-                slidesPerView={3}
-                centeredSlides={true}
-                spaceBetween={30}
-                pagination={{
-                    type: "fraction",
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide style={{ backgroundImage: `url(${img1})` }} onClick={() => handleBackgroundImg(img1)}>Slide 1</SwiperSlide>
-                <SwiperSlide style={{ backgroundImage: `url(${img2})` }} onClick={() => handleBackgroundImg(img2)}>Slide 2</SwiperSlide>
-                <SwiperSlide style={{ backgroundImage: `url(${img3})` }} onClick={() => handleBackgroundImg(img3)}>Slide 3</SwiperSlide>
-                <SwiperSlide style={{ backgroundImage: `url(${img2})` }} onClick={() => handleBackgroundImg(img2)}>Slide 4</SwiperSlide>
-                <SwiperSlide><img src={img1}/><h2 className="a">sajek</h2></SwiperSlide>
-            </Swiper>
-        </div>
+        <Row>
+            <Col xs={12} md={5} style={{ zIndex: '4' }} className="p-5">
+                <h1 className="text-light display-5 fw-bold">{name}</h1>
+                <p className="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nihil fuga unde vitae qui beatae tenetur, facere amet repellat quis dolores obcaecati iusto ea laudantium praesentium. Ea dolor debitis iure?</p>
+                <Button variant="warning">Booking</Button>
+            </Col>
+            <Col xs={12} md={7}>
+                <Swiper
+                    onSwiper={setSwiperRef}
+                    slidesPerView={3}
+                    centeredSlides={true}
+                    spaceBetween={30}
+                    pagination={{
+                        type: "fraction",
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide style={{ backgroundImage: `url(${img1})` }} onClick={() => handleBackgroundImg(img1)}>Slide 1</SwiperSlide>
+
+                    {
+                        travels.map(travel => <SwiperSlide
+                            onClick={() => handleBackgroundImg(travel)}
+                            key={travel.id}>
+                            <img src={travel.image_url} />
+                            <h2 className="a">{travel.name}</h2>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </Col>
+        </Row>
     );
 };
 
---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Main = () => {
+    const travels = useLoaderData();
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Main;
